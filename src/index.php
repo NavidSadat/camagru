e85b319b9ff7ea06454bddbc26863c4a0373c80e
NavidSadat <saydn.ebox@gmail.com>
2025-04-28 15:46:09 +0200

    adding the mvc

--- src/index.php
+++ src/index.php
@@ -1,3 +1,6 @@
 <?php
-echo'<h1>my first ever succes to php-fpm with nginx</h1>'
+include "nav.php";
+echo'<h1>my first ever succes to php-fpm to do my project camagru</h1>';
+echo'<h1>my first ever succes to php-fpm to do my project camagru</h1>';
+
 ?>
