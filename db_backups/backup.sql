5f3703494d9225f6f14aef75e51c1a6230baa521
NavidSadat <sayedn.ebox@gmail.com>
2025-04-29 17:03:17 +0200

    adding the new folder planing

--- db_backups/backup.sql
+++ db_backups/backup.sql
@@ -16,28 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user_sign`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `user_sign`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_sign` (
-  `id` int(72) NOT NULL,
-  `name` varchar(72) NOT NULL,
-  `email` varchar(72) NOT NULL,
-  `token` text NOT NULL,
-  `pass` varchar(72) NOT NULL
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_sign`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `user_sign` WRITE;
-/*!40000 ALTER TABLE `user_sign` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_sign` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-29 11:37:38
+-- Dump completed on 2025-04-29 15:02:29
--- src/index.php
+++ src/index.php
@@ -1,7 +1,4 @@
 <?php
 include "nav.php";
 echo'<h1>my first ever succes to php-fpm to do my project camagru</h1>';
-echo'<h1>my first ever succes to php-fpm to do my project camagru</h1>';
-echo'<h1>my first ever succes to php-fpm to do my project camagru</h1>';
-
 ?>
